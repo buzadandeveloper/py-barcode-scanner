ec1c21f69ddb66b29eb479a79d647b4d6d1b456f
Buza Dan <buzadandeveloper@gmail.com>
2024-12-01 19:58:20 +0200

    ch

--- main.py
+++ main.py
@@ -1,13 +1,14 @@
+ 
 from flask import Flask, request, jsonify, render_template
-from flask_cors import CORS
+from flask_cors import CORS  # Importă CORS
 import base64
 from PIL import Image
 from io import BytesIO
 import zxing
 
-app = Flask(__name__)
+app = Flask(__name__, template_folder='barcodeScanner/templates')
 
-# Enable CORS
+# Activează CORS pentru toate rutele
 CORS(app)
 
 @app.route('/')
@@ -17,24 +18,22 @@ def index():
 @app.route('/send-image', methods=['POST'])
 def send_image():
     try:
-        # Receive the JSON with the base64 image data
         data = request.json
         base64_image_string = data.get('base64')
 
         if not base64_image_string:
             raise ValueError("No image data provided")
 
-        # Debug: Print part of the base64 string to check if it's correctly received
-        print(f"Received base64 string (first 100 chars): {base64_image_string[:100]}")
-
-        # Decode the base64 string to get the image data
         image_data = base64.b64decode(base64_image_string)
         image = Image.open(BytesIO(image_data))
 
-        # Use ZXing to decode the barcode from the image directly in memory (no file saving)
-        reader = zxing.BarCodeReader()
-        barcode = reader.decode(BytesIO(image_data))  # Read directly from the in-memory image data
+        # Salvează imaginea
+        image_path = "decoded_image.jpg"
+        image.save(image_path)
 
+        # Scanează imaginea pentru datele codului de bare
+        reader = zxing.BarCodeReader()
+        barcode = reader.decode(image_path)
         if barcode:
             decoded_text = barcode.parsed
         else:
@@ -42,8 +41,7 @@ def send_image():
 
         return jsonify({'message': 'Image successfully scanned.', 'decodedText': decoded_text}), 200
     except Exception as e:
-        print(f"Error: {str(e)}")  # Log the error for debugging
         return jsonify({'error': str(e)}), 400
 
 if __name__ == '__main__':
-    app.run()
+    app.run(debug=True)
